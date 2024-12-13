bbef7c503429e0a6994c9c3ba619ca490401b9d3
nikhat085@gmail.com <nikhat085@gmail.com>
2024-12-13 14:11:31 +0530

    Add package.json HelloZephyr.c Addition.c

---
+++ Addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int  Add, a, b;
+    a= 10;
+    b= 20;
+    Add = a + b;
+    printf("Adds t0 %d\n", Add);
+    return 0;
+}
---
+++ HelloZephyr.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    // This is a single-line comment
+    printf("I'm here in 'Zephyr'\n");
+    return 0;
+}
